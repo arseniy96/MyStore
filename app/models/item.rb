fa852e24574f75729abba1028df8c4aeef1399e9
arseniy96 <arsenzhar@gmail.com>
2016-03-31 23:41:19 +0300

    Add callbacks to model Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ActiveRecord::Base
   validates :price, numericality: {greater_than: 0, allow_nill: true}
   validates :name, :description, presence: true
+
+  after_initialize {puts "initialized"} # Item.new
+  after_save {puts "saved"} # Item.save
+  after_create {puts "created"}
+  after_update {puts "updated"}
+  after_destroy {puts "destroyed"} # item.destroy
+
 end
